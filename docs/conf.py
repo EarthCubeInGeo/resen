a7fed272bec63bcc2c61b8012bb9a33d90904a96
Leslie Lamarche <leslie.lamarche@sri.com>
2019-05-13 13:38:32 -0700

    Add master_doc explicitly to config.py.

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,7 @@ author = 'A. Reimer'
 # The full version, including alpha/beta/rc tags
 release = 'v2019.1.0rc1'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
